d478ae60a2fd7a6add81b5996e8e99664209a809
Alyssa Haroldsen <kupiakos@google.com>
2021-12-01 12:21:32 -0800

    Pre-read inputs and read from stdin if it doesn't exist

--- common/lib.rs
+++ common/lib.rs
@@ -1,4 +1,4 @@
-use std::{io::{BufReader, BufRead}, fs::File, path::Path};
+use std::{io::{self, Read}, fs::File, path::Path};
 
 #[macro_export]
 macro_rules! get_input {
@@ -6,17 +6,27 @@ macro_rules! get_input {
         $crate::read_input(std::path::Path::new(&format!("inputs/{}.txt", module_path!())))
     };
     (lines) => {
-        {
-            use std::io::BufRead;
-            $crate::get_input!().lines().map(|line| line.expect("invalid line"))
-        }
+        $crate::get_input!().lines()
     };
     (parsed) => {
         $crate::get_input!(lines).map(|line| line.parse().expect("invalid parse"))
     };
 }
 
-pub fn read_input(path: &Path) -> impl BufRead {
-    let f = File::open(path).expect("cannot open input");
-    BufReader::new(f)
+/// shrlorp an input path or read from stdin if it doesn't exist
+pub fn read_input(path: &Path) -> String {
+    match File::open(path) {
+        Ok(mut f) => {
+            let mut s = String::new();
+            f.read_to_string(&mut s).expect("could not read file");
+            s
+        },
+        Err(e) if e.kind() == io::ErrorKind::NotFound => {
+            println!("input file {} not found - enter your input and press Ctrl-D when done", path.display());
+            let mut s = String::new();
+            io::stdin().lock().read_to_string(&mut s).expect("could not read stdin");
+            s
+        },
+        Err(e) => panic!("Unknown error reading input: {:?}", e),
+    }
 }
