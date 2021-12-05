72946e8c8e312304a9ab882fdd5a9b67806ea508
Alyssa Haroldsen <kupiakos@gmail.com>
2021-12-04 22:27:10 -0800

    Day 5

--- common/lib.rs
+++ common/lib.rs
@@ -1,9 +1,19 @@
-use std::{io::{self, Read}, fs::File, path::Path, fmt::{Debug, Display}};
+use std::{
+    fmt::{self, Debug, Display, Write},
+    fs::File,
+    io::{self, Read},
+    path::Path,
+};
+
+use ndarray::Array2;
 
 #[macro_export]
 macro_rules! get_input {
     () => {
-        $crate::read_input(std::path::Path::new(&format!("inputs/{}.txt", module_path!())))
+        $crate::read_input(std::path::Path::new(&format!(
+            "inputs/{}.txt",
+            module_path!()
+        )))
     };
     (lines) => {
         $crate::get_input!().lines()
@@ -20,13 +30,19 @@ pub fn read_input(path: &Path) -> String {
             let mut s = String::new();
             f.read_to_string(&mut s).expect("could not read file");
             s
-        },
+        }
         Err(e) if e.kind() == io::ErrorKind::NotFound => {
-            println!("input file {} not found - enter your input and press Ctrl-D when done", path.display());
+            println!(
+                "input file {} not found - enter your input and press Ctrl-D when done",
+                path.display()
+            );
             let mut s = String::new();
-            io::stdin().lock().read_to_string(&mut s).expect("could not read stdin");
+            io::stdin()
+                .lock()
+                .read_to_string(&mut s)
+                .expect("could not read stdin");
             s
-        },
+        }
         Err(e) => panic!("Unknown error reading input: {:?}", e),
     }
 }
@@ -34,11 +50,33 @@ pub fn read_input(path: &Path) -> String {
 pub struct PanicOnError;
 
 impl Debug for PanicOnError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str("call From<T> to use correctly")
     }
 }
 
 impl<T: Display> From<T> for PanicOnError {
-    fn from(error: T) -> PanicOnError { panic!("error: {}", error) }
+    fn from(error: T) -> PanicOnError {
+        panic!("error: {}", error)
+    }
+}
+
+pub struct Dots<'a, T>(pub &'a Array2<T>);
+impl<'a, T> Display for Dots<'a, T>
+where
+    T: Display + num_traits::Zero,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in self.0.rows() {
+            for e in row {
+                if e.is_zero() {
+                    f.write_char('.')?;
+                } else {
+                    Display::fmt(e, f)?;
+                }
+            }
+            f.write_char('\n')?;
+        }
+        Ok(())
+    }
 }
--- days/05.rs
+++ days/05.rs
@@ -1,3 +1,143 @@
+use std::{cmp::Ordering, ops::RangeInclusive, str::FromStr};
+
+use common::{get_input, Dots, PanicOnError};
+use ndarray::{s, Array2};
+
+#[derive(Clone, Debug)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+impl FromStr for Position {
+    type Err = PanicOnError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (x, y) = s.split_once(",").unwrap();
+        let (x, y) = (x.parse()?, y.parse()?);
+        Ok(Position { x, y })
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Line {
+    from: Position,
+    to: Position,
+}
+enum LineOrientation {
+    Horizontal,
+    Vertical,
+    Diagonal { rising: bool },
+}
+
+impl Line {
+    fn normalize(mut self) -> Line {
+        // left to right, then top to bottom
+        let swap = match self.from.x.cmp(&self.to.x) {
+            Ordering::Equal => self.from.y < self.to.y,
+            Ordering::Less => false,
+            Ordering::Greater => true,
+        };
+        if swap {
+            std::mem::swap(&mut self.from, &mut self.to)
+        };
+        self
+    }
+
+    fn orientation(&self) -> LineOrientation {
+        if self.from.x == self.to.x {
+            LineOrientation::Vertical
+        } else if self.from.y == self.to.y {
+            LineOrientation::Horizontal
+        } else {
+            assert!(
+                (self.from.x - self.to.x).abs() == (self.from.y - self.to.y).abs(),
+                "line {:?} not 45 degrees",
+                self
+            );
+            LineOrientation::Diagonal {
+                rising: self.from.y > self.to.y,
+            }
+        }
+    }
+}
+impl FromStr for Line {
+    type Err = PanicOnError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (from, to) = s.split_once(" -> ").unwrap();
+        let (from, to) = (from.parse()?, to.parse()?);
+        Ok(Line { from, to }.normalize())
+    }
+}
+
+fn line_range(from: i32, to: i32) -> RangeInclusive<usize> {
+    let (from, to) = (from as usize, to as usize);
+    if from > to {
+        to..=from
+    } else {
+        from..=to
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Input {
+    lines: Vec<Line>,
+    max_size: usize,
+}
+
+fn part1(input: &Input) -> usize {
+    // 1 MiB isn't that big
+    let mut map: Array2<i32> = Array2::zeros((input.max_size, input.max_size));
+    for line in &input.lines {
+        match line.orientation() {
+            LineOrientation::Horizontal => {
+                let mut row = map.row_mut(line.from.y as usize);
+                let mut line_content = row.slice_mut(s![line_range(line.from.x, line.to.x)]);
+                line_content += 1;
+            }
+            LineOrientation::Vertical => {
+                let mut col = map.column_mut(line.from.x as usize);
+                let mut line_content = col.slice_mut(s![line_range(line.from.y, line.to.y)]);
+                line_content += 1;
+            }
+            LineOrientation::Diagonal { .. } => (),
+        }
+    }
+    map.into_iter().filter(|&x| x > 1).count()
+}
+
+fn part2(input: &Input) -> usize {
+    let mut map: Array2<i32> = Array2::zeros((input.max_size, input.max_size));
+    for line in &input.lines {
+        match line.orientation() {
+            LineOrientation::Horizontal => {
+                let mut row = map.row_mut(line.from.y as usize);
+                let mut line_content = row.slice_mut(s![line_range(line.from.x, line.to.x)]);
+                line_content += 1;
+            }
+            LineOrientation::Vertical => {
+                let mut col = map.column_mut(line.from.x as usize);
+                let mut line_content = col.slice_mut(s![line_range(line.from.y, line.to.y)]);
+                line_content += 1;
+            }
+            LineOrientation::Diagonal { rising } => {
+                let mut y = line.from.y;
+                for x in line.from.x..=line.to.x {
+                    map[(y as usize, x as usize)] += 1;
+                    y += if rising { -1 } else { 1 };
+                }
+            }
+        }
+    }
+    println!("map:\n{}", Dots(&map));
+    map.into_iter().filter(|&x| x > 1).count()
+}
+
 fn main() {
-    println!("hello world");
+    let input = Input {
+        lines: get_input!(parsed).collect(),
+        max_size: 1000,
+    };
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
