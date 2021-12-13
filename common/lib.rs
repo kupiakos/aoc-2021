b14c7e979fcd95c1370ecba14ebca091b600a40f
Alyssa Haroldsen <kupiakos@gmail.com>
2021-12-12 23:43:00 -0800

    Day 8

--- common/lib.rs
+++ common/lib.rs
@@ -1,8 +1,13 @@
+#![warn(unsafe_op_in_unsafe_fn)]
+
 use std::{
     fmt::{self, Debug, Display, Write},
     fs::File,
     io::{self, Read},
+    mem::MaybeUninit,
+    panic::{self, AssertUnwindSafe},
     path::Path,
+    ptr,
 };
 
 use ndarray::Array2;
@@ -78,5 +83,52 @@ where
             f.write_char('\n')?;
         }
         Ok(())
+    }
+}
+
+pub trait ArrayCollect<T> {
+    fn array_collect<const N: usize>(self) -> Option<[T; N]>;
+}
+
+// adapted from stdlib
+unsafe fn array_assume_init<T, const N: usize>(array: [MaybeUninit<T>; N]) -> [T; N] {
+    // SAFETY:
+    // * The caller guarantees that all elements of the array are initialized
+    // * `MaybeUninit<T>` and T are guaranteed to have the same layout
+    // * `MaybeUninit` does not drop, so there are no double-frees
+    // And thus the conversion is safe
+    unsafe {
+        // intrinsics::assert_inhabited::<[T; N]>();
+        (&array as *const _ as *const [T; N]).read()
+    }
+}
+
+impl<I: Iterator> ArrayCollect<I::Item> for I {
+    fn array_collect<const N: usize>(mut self) -> Option<[I::Item; N]> {
+        // yes i promise this is a sound way to do this
+        // https://doc.rust-lang.org/nomicon/unchecked-uninit.html
+        let mut array: [MaybeUninit<I::Item>; N] = unsafe { MaybeUninit::uninit().assume_init() };
+        let mut written_indexes: usize = 0;
+        match panic::catch_unwind(AssertUnwindSafe(|| {
+            for dst in &mut array {
+                dst.write(self.next()?);
+                written_indexes = written_indexes.wrapping_add(1);
+            }
+            Some(())
+        })) {
+            Ok(x) => x?,
+            Err(e) => {
+                // clean up after ourselves
+                for i in 0..written_indexes {
+                    unsafe {
+                        ptr::drop_in_place(array[i].as_mut_ptr());
+                    }
+                }
+                panic::resume_unwind(e)
+            }
+        }
+        // technically [I::Item; N] is a dependently sized type, so transmute
+        // is out.
+        Some(unsafe { array_assume_init(array) })
     }
 }
--- days/08.rs
+++ days/08.rs
@@ -1,3 +1,142 @@
+use std::{
+    collections::HashMap,
+    fmt::{self, Write},
+    str::FromStr,
+};
+
+use common::{get_input, ArrayCollect, PanicOnError};
+
+struct Input {
+    entries: Vec<Entry>,
+}
+
+#[derive(Clone, Copy)]
+struct Signal(u16);
+impl Signal {
+    fn num_set(self) -> u32 {
+        self.0.count_ones()
+    }
+
+    fn num_common_with(self, other: Signal) -> u32 {
+        (self.0 & other.0).count_ones()
+    }
+
+    fn segments_set(self) -> impl Iterator<Item = u8> {
+        let mut x = self.0;
+        std::iter::from_fn(move || {
+            if x == 0 {
+                return None;
+            }
+            let out = x.trailing_zeros() as u8;
+            x &= x - 1;
+            Some(out)
+        })
+    }
+}
+
+impl fmt::Debug for Signal {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for x in self.segments_set() {
+            f.write_char(char::from_u32((x + b'a').into()).ok_or(fmt::Error)?)?;
+        }
+        Ok(())
+    }
+}
+
+impl FromStr for Signal {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Signal(s.as_bytes().iter().try_fold(
+            0,
+            |acc, &c| match c - b'a' {
+                bit @ 0..=7 => Ok(acc | 1 << (bit as u16)),
+                _c => Err(()),
+            },
+        )?))
+    }
+}
+
+#[derive(Debug)]
+struct Entry {
+    signal_patterns: [Signal; 10],
+    output_value: [Signal; 4],
+}
+
+fn parse_signals<const N: usize>(s: &str) -> Option<[Signal; N]> {
+    // todo: no panic
+    s.split(' ')
+        .map(|x| x.parse::<Signal>().unwrap())
+        .array_collect()
+}
+
+impl FromStr for Entry {
+    type Err = PanicOnError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (signal_patterns, output_value) = s.split_once(" | ").unwrap();
+        let signal_patterns = parse_signals(signal_patterns).unwrap();
+        let output_value = parse_signals(output_value).unwrap();
+
+        Ok(Entry {
+            signal_patterns,
+            output_value,
+        })
+    }
+}
+
+fn part1(input: &Input) -> usize {
+    input
+        .entries
+        .iter()
+        .flat_map(|i| i.output_value.iter().copied())
+        .map(Signal::num_set)
+        .filter(|&num_set| match num_set {
+            2 | 3 | 4 | 7 => true,
+            _ => false,
+        })
+        .count()
+}
+
+fn part2(input: &Input) -> i64 {
+    let mut out = 0;
+    for entry in &input.entries {
+        let mut counts = HashMap::new();
+        for unique_signal in entry.signal_patterns {
+            counts
+                .entry(unique_signal.num_set() as usize)
+                .or_insert(Signal(0))
+                .0 |= unique_signal.0;
+        }
+        let one: Signal = counts[&2];
+        let four: Signal = counts[&4];
+
+        let mut value = 0;
+        for o in entry.output_value {
+            let n = match (o.num_set(), o.num_common_with(one), o.num_common_with(four)) {
+                (2, 2, 2) => 1,
+                (5, 1, 2) => 2,
+                (5, 2, 3) => 3,
+                (4, 2, 4) => 4,
+                (5, 1, 3) => 5,
+                (6, 1, 3) => 6,
+                (3, 2, 2) => 7,
+                (7, 2, 4) => 8,
+                (6, 2, 4) => 9,
+                (6, 2, 3) => 0,
+                (_, _, _) => panic!("invalid signal {:?}", o),
+            };
+            value = value * 10 + n;
+        }
+        out += value;
+    }
+    out
+}
+
 fn main() {
-    println!("hello world");
+    let input = Input {
+        entries: get_input!(parsed).collect(),
+    };
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
